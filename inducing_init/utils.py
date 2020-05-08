7ede768117b02c6e3beacb2020fde3ebe49843cf
DavidBurt <drb62@cam.ac.uk>
2020-05-08 19:47:15 +0100

    deal with issue when all probs are numerically 0 for sampling

--- inducing_init/utils.py
+++ inducing_init/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.linalg
+import warnings
 np.seterr(all='raise')
 
 
@@ -7,6 +8,9 @@ def sample_discrete(unnormalized_probs):
     unnormalized_probs = np.clip(unnormalized_probs, 0, None)
     N = unnormalized_probs.shape[0]
     normalization = np.sum(unnormalized_probs)
+    if normalization == 0:  # if all of the probabilities are numerically 0, sample uniformly
+        warnings.warn("Trying to sample discrete distribution with all 0 weights")
+        return np.random.choice(a=N, size=1)[0]
     probs = unnormalized_probs / normalization
     return np.random.choice(a=N, size=1, p=probs)[0]
 
